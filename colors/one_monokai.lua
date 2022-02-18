d22d0c0ad5d2da23f36ae574f7eeff67bb2d4d0f
cpea2506 <vienpro2506@gmail.com>
2022-02-19 02:06:44 +0700

    colors autoload: refactor vim to lua

---
+++ colors/one_monokai.lua
@@ -0,0 +1,5 @@
+package.loaded["one_monokai"] = nil
+package.loaded["one_monokai.themes"] = nil
+package.loaded["one_monokai.colors"] = nil
+
+require("one_monokai").setup()
--- colors/one_monokai.vim
+++
@@ -1,4 +0,0 @@
-lua package.loaded["one_monokai"] = nil
-
-lua require("one_monokai").setup()
-
