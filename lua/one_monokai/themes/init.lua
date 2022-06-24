998f1d72779ebc210962d93ad187aa869b2a0bc0
Do Vien <vienpro2506@gmail.com>
2022-06-25 02:32:44 +0700

    themes: add highlight for LspInstaller

--- lua/one_monokai/themes/init.lua
+++ lua/one_monokai/themes/init.lua
@@ -105,6 +105,16 @@ function themes:new()
         LspReferenceText = { bg = colors.vulcan, bold = true },
         LspReferenceWrite = { bg = colors.vulcan, bold = true },
 
+        -- lsp installer
+        LspInstallerGreen = { fg = colors.green },
+        LspInstallerHeader = { fg = colors.light_black, bg = colors.cyan, bold = true },
+        LspInstallerHeaderHelp = { fg = colors.light_black, bg = colors.yellow, bold = true },
+        LspInstallerVaderSaber = { fg = colors.pink, bold = true },
+        LspInstallerVersionCheckLoaderDone = { fg = colors.light_black, bg = colors.green, bold = true },
+        LspInstallerOrange = { fg = colors.orange },
+        LspInstallerHighlighted = { fg = colors.pink },
+        LspInstallerLink = { fg = colors.aqua },
+
         -- dashboard
         DashboardHeader = { fg = colors.peanut },
         DashboardCenter = { fg = colors.roman },
