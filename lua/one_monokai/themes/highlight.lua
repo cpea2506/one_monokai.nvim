128c2aa06804c2ec2a7bae377aa0002d6f681aa8
CPea <vienpro2506@gmail.com>
2022-07-14 03:35:27 +0700

    feat(highlight): add document and extend function

--- lua/one_monokai/themes/highlight.lua
+++ lua/one_monokai/themes/highlight.lua
@@ -1,11 +1,15 @@
 local highlight = { groups = {} }
 
+---Create new highlight instance with defined groups
+---@param groups table|nil #list of highlight groups
+---@return table #highlight instance
 function highlight:new(groups)
     self.groups = groups
 
     return self
 end
 
+---Set highlight
 function highlight:set()
     for group, attrs in pairs(self.groups) do
         local status_ok, err = pcall(vim.api.nvim_set_hl, 0, group, attrs)
@@ -14,6 +18,15 @@ function highlight:set()
             error(string.format("themes(%s): %s", group, err), 0)
         end
     end
+end
+
+---Extend current highlight groups with user's highlight groups
+---@param user_groups any
+---@return table #highlight instance
+function highlight:extend(user_groups)
+    local extended_groups = vim.tbl_deep_extend("force", self.groups, user_groups)
+
+    return self:new(extended_groups)
 end
 
 return highlight
