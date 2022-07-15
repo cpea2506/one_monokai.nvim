9a9540c8047b60dc2e6440d367a9c468f5e84dcd
CPea <vienpro2506@gmail.com>
2022-07-15 18:33:06 +0700

    fix(highlight): create new instance each time `new` is called

--- lua/one_monokai/themes/highlight.lua
+++ lua/one_monokai/themes/highlight.lua
@@ -1,18 +1,25 @@
 local highlight = { groups = {} }
 
+highlight.__index = highlight
+
 ---Create new highlight instance with defined groups
 ---@param groups table|nil #list of highlight groups
 ---@return table #highlight instance
 function highlight:new(groups)
-    self.groups = groups
+    local object = {}
 
-    return self
+    setmetatable(object, self)
+    object.groups = groups
+
+    return object
 end
+
+local set_hl = vim.api.nvim_set_hl
 
 ---Set highlight
 function highlight:set()
     for group, attrs in pairs(self.groups) do
-        local status_ok, err = pcall(vim.api.nvim_set_hl, 0, group, attrs)
+        local status_ok, err = pcall(set_hl, 0, group, attrs)
 
         if not status_ok then
             error(string.format("themes(%s): %s", group, err), 0)
@@ -21,12 +28,12 @@ function highlight:set()
 end
 
 ---Extend current highlight groups with user's highlight groups
----@param user_groups any
+---@param user_groups any[] #user defined highlight groups
 ---@return table #highlight instance
 function highlight:extend(user_groups)
     local extended_groups = vim.tbl_deep_extend("force", self.groups, user_groups)
 
-    return self:new(extended_groups)
+    return highlight:new(extended_groups)
 end
 
 return highlight
