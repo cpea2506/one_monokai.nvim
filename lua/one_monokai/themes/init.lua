237f77de5ad1fc23ccd06a6a5e649329ef27872f
CPea <vienpro2506@gmail.com>
2022-09-24 23:57:18 +0700

    perf(themes): index user themes once

--- lua/one_monokai/themes/init.lua
+++ lua/one_monokai/themes/init.lua
@@ -8,15 +8,17 @@ local themes = {}
 function themes.load()
     local colors = require "one_monokai.colors"
     local default = highlight:new(groups.get(colors, config.options.transparent))
+    local user_themes = config.options.themes(colors)
 
-    if vim.tbl_isempty(config.options.themes {}) then
+    -- set default if user has no custom themes
+    if vim.tbl_isempty(user_themes) then
         default:set()
 
         return
     end
 
-    -- extend with user config
-    local extended = default:extend(config.options.themes(colors))
+    -- extend default with user config
+    local extended = default:extend(user_themes)
     local set_theme_ok, err = pcall(function()
         extended:set()
     end)
