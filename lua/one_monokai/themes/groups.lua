234ac02077cf08834eb0c505981cb1bdb7a2f933
CPea <vienpro2506@gmail.com>
2022-10-06 21:34:33 +0700

    feat(themes): more highlight for nvimtree

--- lua/one_monokai/themes/groups.lua
+++ lua/one_monokai/themes/groups.lua
@@ -240,9 +240,28 @@ function groups.get(colors, transparent)
         DashboardHeader = { fg = colors.peanut },
 
         -- nvimtree
+        NvimTreeBookmark = { fg = colors.yellow },
+        NvimTreeEmptyFolderName = { link = "Directory" },
+        NvimTreeExecFile = { fg = colors.yellow, bold = true },
         NvimTreeFolderIcon = { fg = colors.yellow },
+        NvimTreeFolderName = { link = "Directory" },
+        NvimTreeGitDeleted = { link = "DiffDelete" },
         NvimTreeGitDirty = { fg = colors.pink },
+        NvimTreeGitIgnored = { link = "Comment" },
+        NvimTreeGitMerge = { fg = colors.purple },
+        NvimTreeGitNew = { link = "DiffAdd" },
+        NvimTreeGitRenamed = { fg = colors.pink },
         NvimTreeGitStaged = { fg = colors.green },
+        NvimTreeImageFile = { fg = colors.purple, bold = true },
+        NvimTreeIndentMarker = { fg = colors.light_gray },
+        NvimTreeLiveFilterPrefix = { fg = colors.pink, bold = true },
+        NvimTreeLiveFilterValue = { bold = true },
+        NvimTreeOpenedFile = { fg = colors.yellow, bold = true },
+        NvimTreeOpenedFolderName = { link = "Directory" },
+        NvimTreeRootFolder = { fg = colors.pink },
+        NvimTreeSpecialFile = { fg = colors.green, bold = true, underline = true },
+        NvimTreeSymlink = { fg = colors.pink, bold = true },
+        NvimTreeWindowPicker = { fg = colors.white, bg = colors.aqua, bold = true },
 
         -- bufferline
         BufferLineDiagnostic = { fg = colors.dark_gray },
