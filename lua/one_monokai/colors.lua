34f5e0f99bfdccecd266cd60ea4bf67ec7d6c0c8
CPea <cpea2506@gmail.com>
2023-06-22 04:06:22 +0700

    feat(colors): add function to `lighten` or `darken` color

--- lua/one_monokai/colors.lua
+++ lua/one_monokai/colors.lua
@@ -1,8 +1,9 @@
+local colors = {}
+
 local config = require "one_monokai.config"
 local logs = require "one_monokai.logs"
 
-local colors = {}
-
+---@class Palette
 colors.default = {
     fg = "#abb2bf",
     bg = "#282c34",
@@ -28,6 +29,45 @@ colors.default = {
     black = "#1e2024",
     none = "NONE",
 }
+
+---Convert hex value to rgb
+---@param color string
+local function hex2rgb(color)
+    color = string.lower(color)
+
+    return {
+        tonumber(color:sub(2, 3), 16),
+        tonumber(color:sub(4, 5), 16),
+        tonumber(color:sub(6, 7), 16),
+    }
+end
+
+---@param fg string foreground color
+---@param bg string background color
+---@param alpha number number between 0 and 1.
+---@source: https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/util.lua#L9-L37
+local function blend(fg, bg, alpha)
+    local bg_rgb = hex2rgb(bg)
+    local fg_rgb = hex2rgb(fg)
+
+    local blend_channel = function(i)
+        local ret = (alpha * fg_rgb[i] + ((1 - alpha) * bg_rgb[i]))
+
+        return math.floor(math.min(math.max(0, ret), 255) + 0.5)
+    end
+
+    return ("#%02x%02x%02x"):format(blend_channel(1), blend_channel(2), blend_channel(3))
+end
+
+---@param alpha number number between 0 and 1
+function string:darken(alpha)
+    return blend(self, "#000000", alpha)
+end
+
+---@param alpha number number between 0 and 1
+function string:lighten(alpha)
+    return blend(self, "#ffffff", alpha)
+end
 
 ---Check if color is valid before setting it
 ---@param name string #name of the color
@@ -56,18 +96,13 @@ local function is_valid_color(name, value)
     return true
 end
 
-local user_colors = config.colors
+---@type Palette
+colors.extended = vim.deepcopy(colors.default)
 
-if vim.tbl_isempty(user_colors) then
-    return colors.default
-end
-
-for name, value in pairs(user_colors) do
-    if not is_valid_color(name, value) then
-        return colors.default
+for name, value in pairs(config.colors) do
+    if is_valid_color(name, value) then
+        colors.extended[name] = value
     end
 end
-
-colors.extended = vim.tbl_extend("force", colors.default, user_colors)
 
 return colors.extended
