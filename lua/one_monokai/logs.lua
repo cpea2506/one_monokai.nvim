73d9af8029a02844f61f46165f896d8b231217b6
CPea <cpea2506@gmail.com>
2023-11-01 02:20:22 +0700

    chore(logs): remove unused stuffs (#76)

--- lua/one_monokai/logs.lua
+++ lua/one_monokai/logs.lua
@@ -1,8 +1,6 @@
 local logs = {}
 
 logs.error = {
-    msg = "",
-    msg_displayed = false,
     ---Display error message.
     ---
     ---NOTE: This function receives params that will be passed to `string.format`.
@@ -11,10 +9,8 @@ logs.error = {
     notify = function(s, ...)
         local message = string.format(s, ...)
 
-        logs.error.msg = message
-
         vim.schedule(function()
-            logs.error.msg_displayed = vim.notify_once(message, vim.log.levels.ERROR, {
+            vim.notify_once(message, vim.log.levels.ERROR, {
                 title = "One Monokai",
             })
         end)
