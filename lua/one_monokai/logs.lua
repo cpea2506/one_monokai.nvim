f5feea1b29e9f7750d7ec7af2097a518ff8d087c
CPea <cpea2506@gmail.com>
2025-03-30 00:19:11 +0700

    chore: remove redundant colorscheme setting

--- lua/one_monokai/logs.lua
+++ lua/one_monokai/logs.lua
@@ -4,16 +4,14 @@ logs.error = {
     ---Display error message.
     ---
     ---NOTE: This function receives params that will be passed to `string.format`.
-    ---@param s any
+    ---@param s string
     ---@param ... any
     notify = function(s, ...)
         local message = string.format(s, ...)
 
-        vim.schedule(function()
-            vim.notify_once(message, vim.log.levels.ERROR, {
-                title = "One Monokai",
-            })
-        end)
+        vim.notify_once(message, vim.log.levels.ERROR, {
+            title = "One Monokai",
+        })
     end,
 }
 
